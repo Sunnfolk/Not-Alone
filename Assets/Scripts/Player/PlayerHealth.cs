6e7cb2357953b9852fadbb49cf5b0bc5d2f65574
AzeCeres <91120004+AzeCeres@users.noreply.github.com>
2021-09-28 10:02:22 +0200

    Health and some others

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,5 +1,4 @@
-using System;
-using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
 
 namespace Player
@@ -8,21 +7,50 @@ namespace Player
     public class PlayerHealth : MonoBehaviour
     {
         [SerializeField] private int health = 10;
-        private Rigidbody2D m_Rigidbody2D;
+
+        private CoyoteTime m_Dash;
+        private bool m_Invulnerable;
+        [SerializeField] private float invulnerabilityTimer = 0.5f;
 
         private void Awake()
-        { 
-            m_Rigidbody2D = GetComponent<Rigidbody2D>();
-        }
-        /*
-        Update()
         {
-            if ()
+            m_Dash = GetComponent<CoyoteTime>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Damage"))
             {
+                if (m_Dash.isDashing || m_Invulnerable) return;
+                Damage();
                 
             }
+
+            if (other.CompareTag("Death"))
+            {
+                health = 0;
+            }
         }
-        */
-        
+
+        private void Update()
+        {
+            print("Player Health = " + health);
+            
+        }
+
+        private void Damage()
+        {
+            health -= 1;
+            StartCoroutine(Invulnerability());
+        }
+
+        private IEnumerator Invulnerability()
+        {
+            m_Invulnerable = true;
+            print("Invulnerable = " + m_Invulnerable);
+            yield return new WaitForSeconds(invulnerabilityTimer);
+            m_Invulnerable = false;
+            print("Invulnerable = " + m_Invulnerable);
+        }
     }
 }
