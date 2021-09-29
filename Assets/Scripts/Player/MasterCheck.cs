190fd21baba6a82cb40f2b637232c675a7769da6
AzeCeres <91120004+AzeCeres@users.noreply.github.com>
2021-09-29 10:49:26 +0200

    Camera Bug
    Plus imported Boss Sheets

--- Assets/Scripts/Player/MasterCheck.cs
+++ Assets/Scripts/Player/MasterCheck.cs
@@ -8,14 +8,14 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerAnimator))]
 [RequireComponent(typeof(PlayerAttack))]
 [RequireComponent(typeof(PlayerAudio))]
-[RequireComponent(typeof(player))]
+
 
 public class MasterCheck : MonoBehaviour
 {
     
     
     // Start is called before the first frame update
-    void Start()
+    
 
     // Update is called once per frame
     void Update()
