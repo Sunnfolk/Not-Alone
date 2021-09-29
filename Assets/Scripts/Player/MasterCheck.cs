eceed998a4a31e4ebbb6d37e9f7dc5bc13bb4570
AslakDB <Askelakken@gmail.com>
2021-09-29 10:08:37 +0200

    master CHack start and changes to walljump
    walljump is having glitches

---
+++ Assets/Scripts/Player/MasterCheck.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerJump))]
+[RequireComponent(typeof(PlayerColliders))]
+[RequireComponent(typeof(PlayerAnimator))]
+[RequireComponent(typeof(PlayerAttack))]
+[RequireComponent(typeof(PlayerAudio))]
+[RequireComponent(typeof(player))]
+
+public class MasterCheck : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player/WallJump.cs
+++ Assets/Scripts/Player/WallJump.cs
@@ -15,7 +15,6 @@ namespace Player
         private Rigidbody2D m_Rigidbody2D;
         public LayerMask groundMask;
         private bool isGrounded;
-       // private bool jump;
         private bool isTouchingLeft;
         private bool isTouchingRight;
         private bool wallJumping;
@@ -35,16 +34,16 @@ namespace Player
         void Update()
         {
             var position = gameObject.transform.position;
-            isGrounded = Physics2D.OverlapBox(new Vector2((float) (position.x), (float) (position.y - 0.725)),
+            isGrounded = Physics2D.OverlapBox(new Vector2((position.x), (float) (position.y - 0.725)),
                 new Vector2(0.15f, 0.8f), 0f, groundMask);
         
             isTouchingLeft = Physics2D.OverlapBox(new Vector2( (position.x - 0.5f), (position.y)),
                 new Vector2(0.2f, 1.25f), 0f, groundMask);
-            print("isTouchingLeft");
+            
 
             isTouchingRight = Physics2D.OverlapBox(new Vector2( (position.x + 0.5f), (position.y)),
                 new Vector2(0.2f, 1.25f), 0f, groundMask);
-            print("isGrounded");
+            
             
 
             if (isTouchingLeft)
@@ -67,11 +66,12 @@ namespace Player
                 m_Rigidbody2D.velocity = new Vector2(walkSpeed * touchingLeftOrRight,jumpSpeed);
             }
 
-           // if (isTouchingLeft || isTouchingRight && !isGrounded)
-           // {
-               // wallSliding = true;
-                
-          // }
+            if (isTouchingLeft || isTouchingRight && !isGrounded)
+            {
+                wallSliding = true;
+                WallSlide();
+            }
+            
 
            
             
@@ -96,6 +96,11 @@ namespace Player
         private void SetWallJumpFalse()
         {
             wallJumping = false;
+        }
+
+        private void WallSlide()
+        {
+            m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, -slideSpeed);
         }
 
         
