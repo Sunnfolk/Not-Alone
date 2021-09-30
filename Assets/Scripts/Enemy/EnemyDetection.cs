84785664fec9b58772127dd885e9e7b3e9966b64
AslakDB <Askelakken@gmail.com>
2021-09-30 10:32:36 +0200

    mye som her blit jobbet med, enemy og litt ui
    summmary

---
+++ Assets/Scripts/Enemy/EnemyDetection.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyDetection : MonoBehaviour
+    {
+        [SerializeField] private Transform player;
+        [SerializeField] private float agroRange;
+        [SerializeField] private float moveSpeed;
+
+        private Rigidbody2D m_Rigidbody2D;
+
+        void Start()
+        {
+            m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // distance to player
+            float distToPlayer = Vector2.Distance(transform.position, player.position);
+            //print("distToPlayer" + distToPlayer);
+
+            if (distToPlayer < agroRange)
+            {
+                //chase player
+                ChasePlayer();
+            }
+            else
+            {
+                //stop Chasing
+                StopChasingPlayer();
+            }
+        }
+
+        
+        void ChasePlayer()
+            {
+                if (transform.position.x < player.position.x)
+                {
+                    // Enemy is on left, move right
+                    m_Rigidbody2D.velocity = new Vector2(moveSpeed, 0);
+                    transform.localScale = new Vector3(-1, 1);
+                }
+                else if (transform.position.x > player.position.x)
+                {
+                    // Enemy is on Right, move Left
+                    m_Rigidbody2D.velocity = new Vector2(-moveSpeed, 0);
+                    transform.localScale = new Vector3(1, 1);
+                }
+            }
+        
+        void StopChasingPlayer()
+        {
+            m_Rigidbody2D.velocity = Vector2.zero;
+        }
+        
+        }
+    }
+
+
---
+++ Assets/Scripts/UI/HeartsHealthVisual.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class HeartsHealthVisual : MonoBehaviour
+    {
+      
+    }
+
+    }
+
